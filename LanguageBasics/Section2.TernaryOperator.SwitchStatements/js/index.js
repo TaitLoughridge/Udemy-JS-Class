8753b55548e2cc8cebd9a5e6e8ed3ccbd6dcff9c
J Tait Loughridge <lochridge08@gmail.com>
2020-09-13 20:41:10 -0500

    finished Ternary Operators and Switch Statements

--- LanguageBasics/Section2.TernaryOperator.SwitchStatements/js/index.js
+++ LanguageBasics/Section2.TernaryOperator.SwitchStatements/js/index.js
@@ -1,1 +1,57 @@
-console.log('hello world!!!');
+/***************************************/
+// Ternary Operators & Switch Statements
+
+let firstName = 'Tait';
+let age = 32;
+
+/***************************************/
+// Ternary Operators
+
+age >= 21 ? console.log(firstName + ' drinks Jack Daniels Whiskey.') : console.log(firstName + ' drinks juice.');
+
+let drink = age >= 21 ? 'Whiskey' : 'juice';
+console.log(drink);
+
+// if/else way to do the above
+// if (age >=21) {
+//     let drink = "Whiskey";
+// } else {
+//     let drink = "juice";
+// }
+
+/****************************************/
+// Switch Statements
+
+let job = 'sculptor';
+switch (job) {
+    case 'teacher':
+    case 'sculptor':
+    case 'instructor':
+        console.log(firstName + ' teaches kids how to sculpt clay');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an uber in Houston');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs home interiors');
+        break;
+    default:
+        console.log(firstName + ' does something?');
+}
+
+
+// reworked the Boolean Logic
+switch (true) {
+    case age < 13:
+        console.log (firstName + ' is a boy.');
+        break;
+    case age >= 13 && age < 20:
+        console.log (firstName + ' is a teenager.');
+        break;
+    case age >= 20 && age < 30:
+        console.log (firstName + ' is a young man.');
+        break;
+    default:
+        console.log (firstName + ' is a man.');
+}
+
